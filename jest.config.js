059f476e80fb5c3324cfe23b1765e09db22775c4
Sonam Serchan <sonam.serchan@objective.com>
2023-07-07 22:03:31 +1000

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-process.env['EMOJIS_FILE_PATH'] = './src/helpers/files/emojis.json';
-
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,1 @@
 export const SUCCESS_MESSAGE: string = 'Successfully processed slack event';
-export const DEFAULT_EMOJIS_FILE_PATH: string = "./helpers/files/emojis.json";
